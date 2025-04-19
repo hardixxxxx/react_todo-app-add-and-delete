eec004b642e617b39e51e732cc39433f66cb1395
Anton Polishchuk <hardixxxxx@gmail.com>
2025-04-19 16:12:50 +0200

    fix

--- src/components/TodoList.tsx
+++ src/components/TodoList.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { TodoItem } from './TodoItem';
 import { Todo } from '../types/Todo';
+import { CSSTransition, TransitionGroup } from 'react-transition-group';
 
 type TodoListProps = {
   todos: Todo[];
@@ -15,17 +16,25 @@ export const TodoList: React.FC<TodoListProps> = React.memo(
   ({ todos, tempTodo, setTodos, setError, todoIdsToDelete, inputTodoRef }) => {
     return (
       <section className="todoapp__main" data-cy="TodoList">
-        {todos.map(todo => (
-          <TodoItem
-            key={todo.id}
-            todo={todo}
-            setTodos={setTodos}
-            setError={setError}
-            todoIdsToDelete={todoIdsToDelete}
-            inputTodoRef={inputTodoRef}
-          />
-        ))}
-        {tempTodo && <TodoItem todo={tempTodo} isLoad={true} />}
+        <TransitionGroup>
+          {todos.map(todo => (
+            <CSSTransition key={todo.id} timeout={300} classNames="item">
+              <TodoItem
+                key={todo.id}
+                todo={todo}
+                setTodos={setTodos}
+                setError={setError}
+                todoIdsToDelete={todoIdsToDelete}
+                inputTodoRef={inputTodoRef}
+              />
+            </CSSTransition>
+          ))}
+          {tempTodo && (
+            <CSSTransition key={0} timeout={300} classNames="temp-item">
+              <TodoItem todo={tempTodo} isLoad={true} />
+            </CSSTransition>
+          )}
+        </TransitionGroup>
       </section>
     );
   },
